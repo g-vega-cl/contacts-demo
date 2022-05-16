7a527861c000f3d89f6971ba344844a9d1f13020
cesar-vega-unosquare <g.vega.cl@gmail.com>
2022-05-16 15:15:55 -0500

    Display contact list

--- src/modules/contacts/ContactList.tsx
+++ src/modules/contacts/ContactList.tsx
@@ -4,13 +4,20 @@ import { useContacts } from './hooks/useContacts';
 
 export const ContactList = () => {
   const { contacts } = useContacts();
-
+  
   return (
     <Stack>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Button variant='outlined'>Add Contact</Button>
         <Typography>Contacts {contacts?.length || 0}</Typography>
       </Toolbar>
+      <div>
+        <ul>
+        {contacts?.map((contact) => {
+          return(<li key={contact.id}>{contact?.data.firstName}</li>)
+        })}
+        </ul>
+      </div>
     </Stack>
   );
 };
